ea8d54abb4eaf642197925bb9d23f62871562754
Brodey Newman <brodeynewman@gmail.com>
2018-11-05 21:31:50 -0500

    adding material

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
 
-const Home = () => (
-  <div>
-    This is home
-    <Link to="docs" path="/docs">Docs</Link>
-  </div>
-);
+const styles = () => ({
+  root: {},
+});
 
-export default Home;
+const Home = (props) => {
+  const { classes } = props;
+
+  return (
+    <div>
+      This is home
+      <Link to="docs" path="/docs">Docs</Link>
+    </div>
+  );
+};
+
+Home.propTypes = {
+  classes: PropTypes.shape({}).isRequired,
+};
+
+export default withStyles(styles)(Home);
