3d16974882e70bf69e3435448f67c85d48399e25
Brodey Newman <brodeynewman@gmail.com>
2018-11-12 20:19:31 -0500

    ci/cd + shell script

---
+++ scripts/deploy.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+function echoBanner {
+  echo "
+  ----------------------------
+  $1
+  ----------------------------
+  "
+}
+
+function makeAwsDir {
+  echoBanner "Creating .aws directory."
+
+  mkdir -p ~/.aws
+
+  cat > ~/.aws/credentials << EOF1
+  [default]
+  aws_access_key_id = $AWS_ACCESS_KEY
+  aws_secret_access_key = $AWS_SECRET_TOKEN
+EOF1
+
+  echo ".aws direction successfully created."
+}
+
+function deploy {
+  makeAwsDir
+  echoBanner "Initiating deploy process"
+
+  aws s3 sync ${TRAVIS_BUILD_DIR}/dist s3://$1
+
+  echo "Deploy process successfully completed."
+}
+
+function init {
+  case $1 in
+    master)
+      deploy $S3_PROD_BUCKET
+      ;;
+    develop)
+      deploy $S3_DEV_BUCKET
+      ;;
+    *)
+      echo "Branch [$1] not included in target deploy branches. Exiting."
+      ;;
+  esac
+}
+
+init $TRAVIS_BUILD_DIR
