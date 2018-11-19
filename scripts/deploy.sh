41d34bc1cec3478582b92175b09b67cc3e209cf9
Brodey Newman <brodeynewman@gmail.com>
2018-11-18 20:45:24 -0500

    deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -12,7 +12,7 @@ function aws {
   curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
   unzip awscli-bundle.zip
   ./awscli-bundle/install -b ~/bin/aws
-  export PATH=~/bin:$PATH
+  sudo ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
 
   makeAwsDir
 }
@@ -35,7 +35,7 @@ function deploy {
   echoBanner "Initiating deploy process."
 
   aws --version
-  yes N | aws s3 sync ${TRAVIS_BUILD_DIR}/dist s3://$1
+  aws s3 sync ${TRAVIS_BUILD_DIR}/dist s3://$1
 
   echo "Deploy process successfully completed."
 }
